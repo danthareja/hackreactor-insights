6abd69d29364eb1daef201ed7869a037cdef51fe
Dan Thareja <danthareja@gmail.com>
2014-10-08 22:19:15 -0700

    adds 404.html

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Module dependencies
 var express = require("express");
 var morgan = require("morgan");
 var cookieParser = require("cookie-parser");
@@ -7,40 +8,35 @@ var session = require("express-session");
 var static = require("serve-static");
 var passport = require("passport");
 var secret = require("./secret");
-var github = require("./github");
+var github = require("./github"); // GitHub passport strategy
 
-// Handles deployment
-var PORT = process.env.PORT || 1337;
-
-// Create our server
+// Create app
 var app = express();
 
 // Configure Express
-app.use(morgan("dev")); // logger
+app.set("port", process.env.PORT || 1337); // handles deployment
+app.use(morgan("dev")); // logger in dev mode
 app.use(cookieParser());
 app.use(bodyParser());
 app.use(methodOverride());
 app.use(session({ secret: secret.EXPRESS_SESSION }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(static(__dirname, "index.html")); // serve index, Angular takes over
+app.use(static(__dirname + "/public", "index.html")); // serve index, Angular takes over
 
-// Github passport routes
+// Github routes
 app.get("/auth/github", passport.authenticate("github"));
-app.get("/auth/github/callback", passport.authenticate("github", { failureRedirect: "/login" }), function(req, res) {
-    // Successful authentication, redirect home.
+app.get("/auth/github/callback", passport.authenticate("github"), function(req, res) {
+    // Successful authentication, redirect back to Angular
     res.redirect("/");
   });
 
 // 404 Every other route
 app.get("*", function(req, res) {
-  res.send(404);
+  res.status(404).sendFile(__dirname + "/404.html");
 });
 
-// Start dat server
-var server = app.listen(PORT, function() {
-  console.log("We're making it happen on port %d", server.address().port);
+// Start server
+app.listen(app.get("port"), function() {
+  console.log("We're making it happen on port %d", app.get("port"));
 });
-
-// Export dat server
-module.exports = app;
