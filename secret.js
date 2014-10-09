843a3c71cd3827d590bd40b051f01f19c400b9ee
Dan Thareja <danthareja@gmail.com>
2014-10-08 22:20:44 -0700

    Delete secret.js

--- secret.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  EXPRESS_SESSION: "secretssecretsarenofuncunlesstheirsharedwitheveryone",
-  GITHUB_CLIENT_ID: "fd3e3a16322113f365ab",
-  GITHUB_CLIENT_SECRET: "61d2671c78ca9e6f53056f9181ee7b071bc8553e"
-};
