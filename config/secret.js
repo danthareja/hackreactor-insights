2917aaff9977362b88348793e4b188d7243b8520
Dan Thareja <danthareja@gmail.com>
2014-11-15 15:39:15 -0800

    (feat) adds personal token and production github token

--- config/secret.js
+++ config/secret.js
@@ -4,9 +4,10 @@
 
 module.exports = {
   github: {
-    clientID: process.env.githubID || "fd3e3a16322113f365ab",
-    clientSecret: process.env.githubSecret || "61d2671c78ca9e6f53056f9181ee7b071bc8553e",
+    clientID: process.env.githubID || "b19c48c5c9c509973d5c",
+    clientSecret: process.env.githubSecret || "b0b8d7086f30c51a5ff2a394414c989e8b565f33",
     callbackURL: process.env.githubCallback || "http://127.0.0.1:1337/auth/github/callback",
+    token: "2b46b34cef88b53a0d5165b09df76b5b54975400"
   },
   session: process.env.SESSION || "secretsecretsarenofununlesstheirsharedwitheveryone",
   db: process.env.MONGOHQ_URL || "mongodb://localhost/hrstats"
