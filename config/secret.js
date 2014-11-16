e2f8d753c1cdef651bbcdae88ab72df15bdc307c
Dan Thareja <danthareja@gmail.com>
2014-11-15 19:37:43 -0800

    (refactor) changes  mongo from User to Org schema and removes passport for hardcoded token

--- config/secret.js
+++ config/secret.js
@@ -1,14 +1,9 @@
 /**
- * App secrets. Make sure this file is in .gitignore
+ * App secrets. Should be environmental variables when deployed
  */
 
 module.exports = {
-  github: {
-    clientID: process.env.githubID || "b19c48c5c9c509973d5c",
-    clientSecret: process.env.githubSecret || "b0b8d7086f30c51a5ff2a394414c989e8b565f33",
-    callbackURL: process.env.githubCallback || "http://127.0.0.1:1337/auth/github/callback",
-    token: "2b46b34cef88b53a0d5165b09df76b5b54975400"
-  },
+  githubToken: process.env.githubToken || "2b46b34cef88b53a0d5165b09df76b5b54975400",
   session: process.env.SESSION || "secretsecretsarenofununlesstheirsharedwitheveryone",
-  db: process.env.MONGOHQ_URL || "mongodb://localhost/hrstats"
+  db: process.env.MONGOHQ_URL || "mongodb://localhost/hrstats",
 };
