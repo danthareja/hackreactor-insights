28b60d0fe225f3d71846eb04e5d0ead76e164e92
Dan Thareja <danthareja@gmail.com>
2014-11-20 00:44:51 -0800

    (refactor) adds preflight checks for getAllRepos

--- services/githubScraper.js
+++ services/githubScraper.js
@@ -29,29 +29,31 @@ github.authenticate({
 
 var testMe = async.seq(
   getOrganization,
-  getOrganizationMembers,
+  getAllMembers,
+  getAllRepos,
   allDone
 );
 
-testMe('hackreactor', function(err, res) {
+testMe('hackreactor', function(err, results) {
   console.log('All done!');
 });
 
-// testMe('hackreactor', function(err, results) {
-//   console.log('All done!');
+// github.repos.getFromUser({user: 'aconnelly'}, function(err, data) {
+//   console.log(data.meta);
 // });
 
+
 // Helper methods
-function saveData(doc, callback) {
+function saveData(doc, next) {
   console.log('--- Calling saveData ---');
   doc.save(function(err, user, numberAffected) {
     if (err) {
       console.log("Error saving data to mongo", err);
-      callback('Error saving data to mongo', null);
+      next('Error saving data to mongo', null);
     }
     else {
       console.log("All data saved to mongo! ", numberAffected, " entries affected. (1 = worked). Moving on to next step..");
-      callback(null, doc);
+      next(null, doc);
     }
   });
 }
@@ -75,7 +77,6 @@ function queryDatabase(query, callback) {
 // Runs the same GitHub API call while there's still more pages to get then runs the callback on an array of all results
 function paginateAndPush(githubFunc, options, callback) {
   console.log('--- Calling paginateAndPush---');
-  // Holds results of all our paginated calls in closure scope
   var result = [];
 
   (function paginate(page) {
@@ -83,7 +84,6 @@ function paginateAndPush(githubFunc, options, callback) {
     // Add the current page number to our options
     options.page = page;
     githubFunc(options, function(err, data) {
-      console.log('Github call returned!');
       if (err) {
         callback(err, null);
         return;
@@ -92,11 +92,10 @@ function paginateAndPush(githubFunc, options, callback) {
       var hasNextPage = github.hasNextPage(data.meta.link); // Handy method provided in our API
       
       // Combine results
-      console.log('combining results - before', result.length);
       result = result.concat(data);
-      console.log('combining results - after', result.length);
 
       if(!hasNextPage) {
+        result.meta = data.meta; // Gives us access to the meta outside of paginate
         // When there's no more pages, invoke our callback on the collection of all data
         callback(null, result);
         return;
@@ -117,15 +116,15 @@ function paginateAndPush(githubFunc, options, callback) {
  * Passes an Organization object to the next step
  */
 
-function getOrganization(name, callback) {
+function getOrganization(name, next) {
   console.log('--- Calling getOrganization for', name, '---');
   checkIfOrganizationExists(name, function(err, existingOrg) {
     if (err) {
-      callback(err, null);
+      next(err, null);
     } else if (existingOrg) {
-      callback(null, existingOrg); // Move to step 2
+      next(null, existingOrg); // Move to step 2
     } else {
-      addNewOrganization(name, callback);
+      addNewOrganization(name, next);
     }
   });
 }
@@ -138,35 +137,35 @@ function checkIfOrganizationExists(name, callback) {
   queryDatabase(query, callback);
 }
 
-function addNewOrganization(name, callback) {
+function addNewOrganization(name, next) {
   console.log('--- Calling addNewOrganization for', name, '---');
   var options = { org: name };
   github.orgs.get(options, function(err, org) {
     if (err) {
-      callback(err, null);
+      next(err, null);
     } else {
       console.log('Results returned from github.org.get', org.login);
       var newOrg = createNewOrganization(org);
-      saveData(newOrg, callback);
+      saveData(newOrg, next);
     }
   });
 }
 
-function createNewOrganization(options) {
+function createNewOrganization(org) {
   return new Organization({
-    username: options.login,
+    username: org.login,
     profile: {
-      display_name: options.name,
-      url: options.html_url,
-      avatar: options.avatar_url,
-      location: options.location,
-      email: options.email,
-      public_repos: options.public_repos,
-      public_gists: options.public_gists,
-      followers: options.followers,
-      following: options.following,
-      created_at: options.created_at,
-      updated_at: options.updated_at
+      display_name: org.name,
+      url: org.html_url,
+      avatar: org.avatar_url,
+      location: org.location,
+      email: org.email,
+      public_repos: org.public_repos,
+      public_gists: org.public_gists,
+      followers: org.followers,
+      following: org.following,
+      created_at: org.created_at,
+      updated_at: org.updated_at
     }
   });
 }
@@ -181,8 +180,8 @@ function createNewOrganization(options) {
  * Passes an Organization object to the next step
  */
 
-function getOrganizationMembers(org, callback) {
-  console.log('--- Calling getOrganizationMembers for', org.username, '---');
+function getAllMembers(org, next) {
+  console.log('--- Calling getAllMembers for', org.username, '---');
   var options = { org: org.username , per_page: 100 };
   
   paginateAndPush(github.orgs.getMembers, options, function(err, allMembers) {
@@ -190,18 +189,18 @@ function getOrganizationMembers(org, callback) {
     
     // Update our entries if there are any new members
     if (org.members.length !== allMembers.length) {
-      console.log('New members found! Adding them...')
-      addNewOrganizationMembers(org, allMembers, callback);
+      console.log('New members found! Adding them...');
+      addNewMembers(org, allMembers, next);
     } else {
-      console.log('No new members, moving on to step 3...')
-      callback(null, org); // Move to step 3
+      console.log('No new members, moving on to step 3...');
+      next(null, org); // Move to step 3
     }
   });
 }
 
 /* * *  STEP 2 HELPERS  * * */
 
-function addNewOrganizationMembers(org, members, callback) {
+function addNewMembers(org, members, next) {
   async.filter(members, isNewMember, function(newMembers) {
     console.log('newMembers back from filter: ', newMembers);
     // newMembers is now only those that didn't exist before in our database
@@ -212,7 +211,7 @@ function addNewOrganizationMembers(org, members, callback) {
         repos: []
       });
     });
-    saveData(org, callback); // Will move to step 3
+    saveData(org, next); // Move on to step 3
   });
 }
 
@@ -227,13 +226,81 @@ function isNewMember(member, callback) {
   });
 }
 
+/**
+ * ======= STEP 3 ========
+ *
+ * Get member repos. This will use the organization passed in from step 2 and
+ * get all repos associated with all members in the organization
+ *
+ * Passes an Organization object to the next step
+ */
 
-// /**
-//  * ======= STEP 3 ========
-//  *
-//  * Goes through each member in the authenticated user's members array and gets all repos associated with each member
-//  * Stores ONLY REPOS UPDATED IN THE LAST WEEK in user.members.[[member]].repos array in mongo. NOTE: Update does not mean a commit was made
-//  */
+function getAllRepos(org, next) {
+  async.each(org.members, getReposForMember, goToNextStep);
+
+  function goToNextStep(err) {
+    if (err) { next(err, null); }
+    saveData(org, next);
+  }
+}
+
+/* * *  STEP 3 HELPERS  * * */
+
+function getReposForMember(member, callback) {
+  var options = {
+    user: member.username,
+    per_page: 100,
+    headers: {
+      'If-None-Match': member.etag
+    }
+  };
+
+  paginateAndPush(github.repos.getFromUser, options, function(err, repos) {
+    console.log('Got all repos for member', member.username); // Could come back with a 304 here
+
+    // Update our entries if there are any new repos
+    if (repos.length) {
+      console.log('New repos found! Adding them...');
+      member.etag = repos.meta.etag; // Update etag
+      addNewReposToMember(member, repos, callback);
+    } else {
+      console.log('No new repos, moving on to next user');
+      callback(null); // Let async.each know we're done with this iteration
+    }
+  });
+}
+
+function addNewReposToMember(member, repos, callback) {
+  async.filter(repos, isNewRepo, function(newRepos) {
+    console.log('newRepos back from filter: ', newRepos);
+    // newRepos is now only those that didn't exist before in our database
+    newRepos.forEach(function(repo) {
+      // Janky
+      if (repo.name) {
+        console.log('adding new repo:', repo.owner.login + '/' + repo.name);
+        repo.name && member.repos.push({
+          name: repo.name,
+          owner: repo.owner.login,
+          updated_at: repo.updated_at,
+          repos: []
+        });
+      }
+    });
+    callback(null); // Let async.each know we're done with this iteration
+  });
+}
+
+// async.filter test function
+function isNewRepo(repo, callback) {
+  var query = { "members.username.repos": repo };
+  queryDatabase(query, function(err, org) {
+    if (org) {
+      callback(false);
+    }
+    callback(true);
+  });
+}
+
 
 // // TODO: rethink ++completed requests
 // function getMemberRepos(org, callback) {
