2b3a7dd7844b95336b5dea4342953fe227660924
Dan Thareja <danthareja@gmail.com>
2014-11-23 22:41:01 -0800

    (fix) gets dates using UTC time instead of local

--- controllers/mongo.js
+++ controllers/mongo.js
@@ -1,120 +1,102 @@
 var Organization = require("../models/Organization");
 
-// Parses stringified stats and makes dates usable by javascript
-var parseStats = function(statString) {
-  // Every so often, GitHub gives us an undefined, just return out with an empty array
-  if (!statString) return [];
-  // If there's nothing there, we'll just assign it to an empty array
-  var stats = JSON.parse(statString.length > 0 ? statString : "[]");
-
-  // Every so often, we get an Object instead of an Array from Github. The rest of our code expects arrays
-  return Array.isArray(stats) ? stats : [];
-};
-
-// Middleware to get the requested org from mongo
+// Middleware that handles database lookup
 exports.getOrganization = function(req, res, next) {
-  org = req.query.org || 'hackreactor'; // Allow users to send custom org over query string
+  // Allow users to send custom org over query string
+  org = req.query.org || 'hackreactor';
   console.log('getting organization data for ', org);
-  // Check to see if organization already exists in our db and create a new one if not
   Organization.findOne({ username: org }, function(err, existingOrg) {
-    console.log("length:", existingOrg.members.length)
     if (existingOrg) {
-      req.org = existingOrg; // Pass on reference to the existing org
+      // Pass on reference to the existing org
+      req.org = existingOrg;
       next();
     } else {
-      res.send(404);
+      res.status(404).end();
     }
   });
 };
 
-/**
- * GET /api/stats/code_frequency
- * Converts all repo's codeFrequency stats stored on user into just those since last Sunday
- * Format for d3 -> [{username, repo_name, additions, deletions, net}, ...]
- */
+
+  /**********************************************************************************
+   *                         GET /api/stats/code_frequency                          *
+   *                                                                                *
+   * Converts all repo's codeFrequency stats into just those since last Sunday      *
+   * Format for d3 -> [{username, repo_name, additions, deletions, net}, ...]       *
+   **********************************************************************************/
+
 
 exports.getCodeFrequency = function(req, res) {
   var stats = [];
 
-  // Ignore any members with empty repos
-  var filtered = req.org.members.filter(function(member) {
-    return member.repos.length > 0;
-  });
-
-  filtered.forEach(function(member) {
+  req.org.members.filter(hasRepos).forEach(function(member) {
     member.repos.forEach(function(repo) {
-      parseStats(repo.stats.codeFrequency).forEach(function(stat) {
-        // console.log("repo: ", repo.name, "date: ", stat[0]);
-        // codeFrequency stats come in tuples [date, additions, deletions]
-        // We want to make sure there's at least some action!
-        if (isLastSaturday(stat[0]) && stat[1] > 0 && stat[2] < 0) {
-          console.log("found a repo:", repo.name, " from this week! it adds " , stat[1] , " lines and removes ", stat[2], " lines for a total of ", stat[1] + stat[2], " new lines ");
-          // d3 friendly format (see above)
-          stats.push({
-            username: member.username,
-            repo: repo.name,
-            additions: stat[1],
-            deletions: -stat[2], // Convert negative to positive
-            net: stat[1] + stat[2]
-          });
-        }
-      });
+      parseStats(repo.stats.codeFrequency).forEach(pushStats);
     });
-  });
-
-  // sorts stats in descending order by number of additions
-  stats = stats.sort(function(a,b) {
-    return b.additions - a.additions;
   });
 
   res.send(stats);
 
-  // Date -> Bool ****THIS NEEDS TO BE FIXED. github is weird with the times it passes into codeFreq. Figure this out****
-  function isLastSaturday(date) {
+  function pushStats(stat) {
+    var date = stat[0];
+    var additions = stat[1];
+    var deletions = stat[2];
+    if (isLastSunday(date) && (additions > 0 || deletions < 0)) {
+      console.log("found a repo:", repo.name, " from this week! it adds " , additions , " lines and removes ", deletions, " lines for a total of ", additions + deletions, " new lines ");
+      stats.push({
+        username: repo.owner,
+        repo: repo.name,
+        additions: additions,
+        deletions: -deletions,
+        net: additions + deletions
+      });
+    }
+  }
+
+  // GitHub Punch card dates each week starting with Sunday, 0:00:00 UTC.
+  function isLastSunday(date) {
     var now = new Date();
-    var today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-    var saturday = new Date(today.setDate(today.getDate()-today.getDay()-1));
-    return date === saturday.setHours(saturday.getHours()+16) / 1000;
+    // Handle any timezone that new Date() is created in
+    var todayUTC = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate()));
+    var sundayUTC = new Date(todayUTC.setDate(todayUTC.getDate()-todayUTC.getDay()-8));
+    return date === sundayUTC.getTime() / 1000;
   }
 };
 
 
-/**
- * GET /api/stats/punch_card
- * Formats authorized users' members array into punch card dataset usable by d3
- * Format for d3 -> [{day, hour, commits, repos}, ...]
- */
+  /**********************************************************************************
+   *                         GET /api/stats/punch_card                              *
+   *                                                                                *
+   * Formats an organization's members array into punch card dataset usable by d3   *
+   * Format for d3 -> [{day, hour, commits, repos}, ...]                            *
+   **********************************************************************************/
+
  
 exports.getPunchCard = function(req, res) {
   var stats = initializeStats();
 
-  // Ignore any members with empty repos
-  var filtered = req.org.members.filter(function(member) {
-    return member.repos.length > 0;
-  });
-
-  filtered.forEach(function(member) {
+  req.org.members.filter(hasRepos).forEach(function(member) {
     member.repos.forEach(function(repo) {
-      parseStats(repo.stats.punchCard).forEach(function(stat, i) {
-        // punchCard stats come in tuples [day, hour, number of commits]
-        // For example, [2, 14, 25] indicates that there were 25 total commits, during the 2:00pm hour on Tuesdays. All times are based on the time zone of individual commits.
-        // We want to make sure there's at least some action!
-        if (stat[2] > 0) {
-          console.log("found a repo:", repo.owner, "/", repo.name, "from this week! at day " , stat[0] , " hour ", stat[1], " with number of commits: ", stat[2]);
-          // d3 friendly format (see above)
-          stats[i].commits += stat[2];
-          stats[i].repos.push({
-            user: repo.owner,
-            repo: repo.name,
-            commits: stat[2]
-          });
-        }
-      });
+      parseStats(repo.stats.punchCard).forEach(pushStats);
     });
   });
 
   res.send(stats);
 
+  // punchCard stats come in triples [day, hour, # of commits]
+  function pushStats(stat, i) {
+    var commits = stat[2];
+    if (commits > 0) {
+      console.log("found a repo:", repo.owner, "/", repo.name, "from this week! at day " , stat[0] , " hour ", stat[1], " with number of commits: ", stat[2]);
+      stats[i].commits += commits;
+      stats[i].repos.push({
+        user: repo.owner,
+        repo: repo.name,
+        commits: commits
+      });
+    }
+  }
+
+  // Github's zero indexed days/hours makes it convenient to initialize in a nested for loop
   function initializeStats(){
     var stats = [];
     for (var day = 0; day < 7; day++) {
@@ -129,4 +111,20 @@ exports.getPunchCard = function(req, res) {
     }
     return stats;
   }
+};
+
+  /* * * * * * * * * * * * * * * * HELPERS * * * * * * * * * * * * * * * * * */
+
+function parseStats(statString) {
+  // Every so often, GitHub gives us an undefined, just return out with an empty array
+  if (!statString) return [];
+  // If there's nothing there, we'll just assign it to an empty array
+  var stats = JSON.parse(statString.length > 0 ? statString : "[]");
+
+  // Every so often, we get an Object instead of an Array from Github. The rest of our code expects arrays
+  return Array.isArray(stats) ? stats : [];
+}
+
+function hasRepos(member) {
+  return member.repos.length > 0;
 }
