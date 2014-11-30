33c16c44d2e77b68ed02f03f4fa74691034661c9
Dan Thareja <danthareja@gmail.com>
2014-11-29 16:34:57 -0800

    Delete secret.js

--- config/secret.js
+++
@@ -1,9 +0,0 @@
-/**
- * App secrets. Should be environmental variables when deployed
- */
-
-module.exports = {
-  githubToken: process.env.githubToken || "a2bd13be2479484d3a3469e98386daecfd0ea760",
-  session: process.env.SESSION || "secretsecretsarenofununlesstheirsharedwitheveryone",
-  db: process.env.MONGOHQ_URL || "mongodb://localhost/hrstats",
-};
